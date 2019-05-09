64db327fd9e56b582b5baa7ae4432c643544c9b1
Manjunath Reddy <manju16832003@gmail.com>
2019-05-09 17:15:27 +0800

    Upload file

--- app.js
+++ app.js
@@ -3,18 +3,46 @@ const express = require('express'),
     port = process.env.PORT || 3000,
     bodyParser = require('body-parser')
 
+var multer  =   require('multer');
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 // routes/controllers
 app.use('/api', require('./routes/index'))
 
+var storage =   multer.diskStorage({
+    // file upload destination
+    destination: function (req, file, callback) {
+      callback(null, './uploads');
+    },
+    filename: function (req, file, callback) {
+      callback(null, file.fieldname + '-' + Date.now());
+    }
+  });
+
+var upload = multer({ storage : storage}).single('avatar');
+
+app.post('/api/upload', (req, res) => {
+    upload(req,res,function(err) {
+        // req.file is the `avatar` file
+        // req.body will hold the text fields, if there were any
+          if(err) {
+              console.log(err)
+              return res.end("Error uploading file.");
+          }
+          res.end("File is uploaded");
+      });
+})
 // catch all
 app.use((req, res) => {
     res.status(404).send({
         url: `${req.originalUrl} not found`
     })
 })
+
+
+
 app.listen(port)
 
 console.log(`RESTful API listening to: ${port}`)
